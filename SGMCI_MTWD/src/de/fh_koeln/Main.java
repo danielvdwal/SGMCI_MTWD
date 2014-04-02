cacd94acb69c567ba1c9183f474508714ecd54b7
danielvdwal <daniel.v.d.wal@codecentric.de>
2014-04-02 15:46:37 +0200

    Added main file

---
+++ SGMCI_MTWD/src/de/fh_koeln/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.fh_koeln;
+
+/**
+ *
+ * @author danielvanderwal
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello SGMCI Members!");
+    }
+    
+}
