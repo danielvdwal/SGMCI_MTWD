bfdc4b18355d4b2c4ffb46a1fbbc0efcba5b6a72
danielvdwal <daniel.v.d.wal@codecentric.de>
2014-05-13 13:30:59 +0200

    added splashscenefactory

---
+++ SGMCI_MTWD/src/de/fh_koeln/sgmci/mtwd/scene/factory/SplashSceneFactory.java
@@ -0,0 +1,20 @@
+package de.fh_koeln.sgmci.mtwd.scene.factory;
+
+import de.fh_koeln.sgmci.mtwd.scene.SplashScene;
+import de.fh_koeln.sgmci.mtwd.scene.IScene;
+import org.mt4j.MTApplication;
+
+/**
+ *
+ * @author Daniel van der Wal
+ * @version 0.1.0
+ */
+public class SplashSceneFactory extends AbstractMTWDSceneFactory {
+
+    @Override
+    public IScene createMTWDScene(MTApplication mtApp, String name) {
+        IScene scene = new SplashScene(mtApp, name);
+        createMTWDSceneContent(scene);
+        return scene;
+    }
+}
