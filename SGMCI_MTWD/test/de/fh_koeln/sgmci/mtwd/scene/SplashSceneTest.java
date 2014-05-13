33276922002a565d7a61028772caace4f5f85ae9
hombre7lobo <r.scherbarth@icloud.com>
2014-05-13 13:32:41 +0200

    Added Splash Scene Added SplashSceneTest

---
+++ SGMCI_MTWD/test/de/fh_koeln/sgmci/mtwd/scene/SplashSceneTest.java
@@ -0,0 +1,27 @@
+package de.fh_koeln.sgmci.mtwd.scene;
+
+import de.fh_koeln.sgmci.mtwd.scene.factory.AbstractMTWDSceneFactory;
+import de.fh_koeln.sgmci.mtwd.scene.factory.RealistCommentingSceneFactory;
+import de.fh_koeln.sgmci.mtwd.scene.factory.SplashSceneFactory;
+import org.mt4j.MTApplication;
+import static org.mt4j.MTApplication.initialize;
+
+/**
+ *
+ * @author danielvanderwal
+ */
+public class SplashSceneTest extends MTApplication {
+
+    public static void main(String[] args) {
+        initialize();
+    }
+
+    @Override
+    public void startUp() {
+        AbstractMTWDSceneFactory factory = new SplashSceneFactory();
+        IScene scene = factory.createMTWDScene(this, "Splash Scene");
+        
+        this.addScene(scene);
+        scene.startScene();
+    }
+}
