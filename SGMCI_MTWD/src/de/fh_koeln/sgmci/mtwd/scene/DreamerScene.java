c4cf03c4b71c7945a05060d67a8fb792e03e8689
danielvdwal <daniel.v.d.wal@codecentric.de>
2014-05-21 12:28:28 +0200

    removed commented code

--- SGMCI_MTWD/src/de/fh_koeln/sgmci/mtwd/scene/DreamerScene.java
+++ SGMCI_MTWD/src/de/fh_koeln/sgmci/mtwd/scene/DreamerScene.java
@@ -17,7 +17,6 @@ import org.mt4j.components.visibleComponents.font.FontManager;
 import org.mt4j.components.visibleComponents.font.IFont;
 import org.mt4j.components.visibleComponents.widgets.MTBackgroundImage;
 import org.mt4j.components.visibleComponents.widgets.MTTextArea;
-import org.mt4j.components.visibleComponents.widgets.buttons.MTSvgButton;
 import org.mt4j.input.gestureAction.TapAndHoldVisualizer;
 import org.mt4j.input.inputProcessors.IGestureEventListener;
 import org.mt4j.input.inputProcessors.MTGestureEvent;
@@ -30,7 +29,7 @@ import org.mt4j.util.math.Vector3D;
 import processing.core.PImage;
 
 /**
- *
+ * 
  * @author Robert Scherbarth, Daniel van der Wal
  * @version 0.2.0
  */
@@ -87,154 +86,6 @@ public class DreamerScene extends AbstractMTWDScene {
         user4Workplace = new DreamerUserWorkplace(mtApp);
         user4Workplace.scale(keyboardScaleFactor, keyboardScaleFactor, keyboardScaleFactor, Vector3D.ZERO_VECTOR);
 
-        /*final MTTextArea user1CurrentTextArea = new MTTextArea(mtApp, ideaFont);
-         user1CurrentTextArea.setExpandDirection(MTTextArea.ExpandDirection.UP);
-         user1CurrentTextArea.setStrokeColor(new MTColor(0, 0, 0, 255));
-         user1CurrentTextArea.setFillColor(new MTColor(205, 200, 177, 255));
-         user1CurrentTextArea.setEnableCaret(true);
-         user1Keyboard.getLeftKeyboard().addChild(user1CurrentTextArea);
-         user1CurrentTextArea.setPositionRelativeToParent(new Vector3D(40, -user1CurrentTextArea.getHeightXY(TransformSpace.LOCAL) * 0.5f));
-         user1Keyboard.addTextInputListener(user1CurrentTextArea);
-
-         final MTRectangle user1Rectangle = new MTRectangle(-100, 0, 30, 30, mtApp);
-         user1Rectangle.translate(new Vector3D(0, 15, 0));
-         user1Rectangle.registerInputProcessor(new TapProcessor(mtApp));
-         user1Rectangle.addGestureListener(TapProcessor.class,
-         new IGestureEventListener() {
-         @Override
-         public boolean processGestureEvent(MTGestureEvent ge) {
-         TapEvent te = (TapEvent) ge;
-         if (te.getId() == TapEvent.GESTURE_DETECTED) {
-         try {
-         controller.createIdea(user1CurrentTextArea.getText());
-         user1CurrentTextArea.setText("");
-         } catch (NoIdeaTextException ex) {
-         // do nothing
-         }
-         }
-         return false;
-         }
-         });
-         user1Keyboard.addChild(user1Rectangle);
-
-         user1Keyboard.setPositionGlobal(new Vector3D(mtApp.width / 2, mtApp.height - height / 2 - 20, 0));
-         */
-        /*user2Keyboard = new SplitKeyboard(mtApp);
-         user2Keyboard.setSpaceBetweenKeyboards(200);
-         user2Keyboard.scale(keyboardScaleFactor, keyboardScaleFactor, keyboardScaleFactor, Vector3D.ZERO_VECTOR);
-
-         final MTTextArea user2CurrentTextArea = new MTTextArea(mtApp, ideaFont);
-         user2CurrentTextArea.setExpandDirection(MTTextArea.ExpandDirection.UP);
-         user2CurrentTextArea.setStrokeColor(new MTColor(0, 0, 0, 255));
-         user2CurrentTextArea.setFillColor(new MTColor(205, 200, 177, 255));
-         user2CurrentTextArea.setEnableCaret(true);
-         user2Keyboard.getLeftKeyboard().addChild(user2CurrentTextArea);
-         user2CurrentTextArea.setPositionRelativeToParent(new Vector3D(40, -user2CurrentTextArea.getHeightXY(TransformSpace.LOCAL) * 0.5f));
-         user2Keyboard.addTextInputListener(user2CurrentTextArea);
-
-         final MTRectangle user2Rectangle = new MTRectangle(-100, 0, 30, 30, mtApp);
-         user2Rectangle.translate(new Vector3D(0, 15, 0));
-         user2Rectangle.registerInputProcessor(new TapProcessor(mtApp));
-         user2Rectangle.addGestureListener(TapProcessor.class,
-         new IGestureEventListener() {
-         @Override
-         public boolean processGestureEvent(MTGestureEvent ge) {
-         TapEvent te = (TapEvent) ge;
-         if (te.getId() == TapEvent.GESTURE_DETECTED) {
-         try {
-         controller.createIdea(user2CurrentTextArea.getText());
-         user2CurrentTextArea.setText("");
-         } catch (NoIdeaTextException ex) {
-         // do nothing
-         }
-         }
-         return false;
-         }
-         });
-         user2Keyboard.addChild(user2Rectangle);
-
-         user2Keyboard.rotateZ(new Vector3D(width / 2, height / 2), 180);
-         user2Keyboard.setPositionGlobal(new Vector3D(mtApp.width / 2, height / 2  + 20, 0));
-
-         user3Keyboard = new SplitKeyboard(mtApp);
-         user3Keyboard.setSpaceBetweenKeyboards(200);
-         user3Keyboard.scale(keyboardScaleFactor, keyboardScaleFactor, keyboardScaleFactor, Vector3D.ZERO_VECTOR);
-
-         final MTTextArea user3CurrentTextArea = new MTTextArea(mtApp, ideaFont);
-         user3CurrentTextArea.setExpandDirection(MTTextArea.ExpandDirection.UP);
-         user3CurrentTextArea.setStrokeColor(new MTColor(0, 0, 0, 255));
-         user3CurrentTextArea.setFillColor(new MTColor(205, 200, 177, 255));
-         user3CurrentTextArea.setEnableCaret(true);
-         user3Keyboard.getLeftKeyboard().addChild(user3CurrentTextArea);
-         user3CurrentTextArea.setPositionRelativeToParent(new Vector3D(40, -user3CurrentTextArea.getHeightXY(TransformSpace.LOCAL) * 0.5f));
-         user3Keyboard.addTextInputListener(user3CurrentTextArea);
-
-         final MTRectangle user3Rectangle = new MTRectangle(-100, 0, 30, 30, mtApp);
-         user3Rectangle.translate(new Vector3D(0, 15, 0));
-         user3Rectangle.registerInputProcessor(new TapProcessor(mtApp));
-         user3Rectangle.addGestureListener(TapProcessor.class,
-         new IGestureEventListener() {
-         @Override
-         public boolean processGestureEvent(MTGestureEvent ge) {
-         TapEvent te = (TapEvent) ge;
-         if (te.getId() == TapEvent.GESTURE_DETECTED) {
-         try {
-         controller.createIdea(user3CurrentTextArea.getText());
-         user3CurrentTextArea.setText("");
-         } catch (NoIdeaTextException ex) {
-         // do nothing
-         }
-         }
-         return false;
-         }
-         });
-         user3Keyboard.addChild(user3Rectangle);
-
-         user3Keyboard.rotateZ(new Vector3D(width / 2, height / 2), 90);
-         user3Keyboard.setPositionGlobal(new Vector3D(height / 2 + 20, mtApp.height / 2, 0));
-
-         user4Keyboard = new SplitKeyboard(mtApp);
-         user4Keyboard.setSpaceBetweenKeyboards(200);
-         user4Keyboard.scale(keyboardScaleFactor, keyboardScaleFactor, keyboardScaleFactor, Vector3D.ZERO_VECTOR);
-
-         final MTTextArea user4CurrentTextArea = new MTTextArea(mtApp, ideaFont);
-         user4CurrentTextArea.setExpandDirection(MTTextArea.ExpandDirection.UP);
-         user4CurrentTextArea.setStrokeColor(new MTColor(0, 0, 0, 255));
-         user4CurrentTextArea.setFillColor(new MTColor(205, 200, 177, 255));
-         user4CurrentTextArea.setEnableCaret(true);
-         user4Keyboard.getLeftKeyboard().addChild(user4CurrentTextArea);
-         user4CurrentTextArea.setPositionRelativeToParent(new Vector3D(40, -user4CurrentTextArea.getHeightXY(TransformSpace.LOCAL) * 0.5f));
-         user4Keyboard.addTextInputListener(user4CurrentTextArea);
-
-         final MTRectangle user4Rectangle = new MTRectangle(-100, 0, 30, 30, mtApp);
-         user4Rectangle.translate(new Vector3D(0, 15, 0));
-         user4Rectangle.registerInputProcessor(new TapProcessor(mtApp));
-         user4Rectangle.addGestureListener(TapProcessor.class,
-         new IGestureEventListener() {
-         @Override
-         public boolean processGestureEvent(MTGestureEvent ge) {
-         TapEvent te = (TapEvent) ge;
-         if (te.getId() == TapEvent.GESTURE_DETECTED) {
-         try {
-         controller.createIdea(user4CurrentTextArea.getText());
-         user4CurrentTextArea.setText("");
-         } catch (NoIdeaTextException ex) {
-         // do nothing
-         }
-         }
-         return false;
-         }
-         });
-         user4Keyboard.addChild(user4Rectangle);
-
-         user4Keyboard.rotateZ(new Vector3D(width / 2, height / 2), -90);
-         user4Keyboard.setPositionGlobal(new Vector3D(mtApp.width - height / 2 - 20, mtApp.height / 2, 0));
-
-         user1Keyboard.setPickable(false);
-         user1Keyboard.removeAllGestureEventListeners();
-         user1Keyboard.unregisterAllInputProcessors();
-         //this.getCanvas().addChild(user1Keyboard);
-         */
         user1Workplace.setPositionGlobal(new Vector3D(mtApp.width / 2, mtApp.height - user1Workplace.getHeightXY(TransformSpace.RELATIVE_TO_PARENT) / 2 - 20, 0));
         this.getCanvas().addChild(user1Workplace);
 
@@ -249,76 +100,6 @@ public class DreamerScene extends AbstractMTWDScene {
         user4Workplace.rotateZ(new Vector3D(user4Workplace.getWidthXY(TransformSpace.RELATIVE_TO_PARENT) / 2, user4Workplace.getHeightXY(TransformSpace.RELATIVE_TO_PARENT) / 2), -90);
         user4Workplace.setPositionGlobal(new Vector3D(mtApp.width - user4Workplace.getHeightXY(TransformSpace.RELATIVE_TO_PARENT) / 2 - 20, mtApp.height / 2, 0));
         this.getCanvas().addChild(user4Workplace);
-
-        /*user2Keyboard.setPickable(false);
-         user2Keyboard.removeAllGestureEventListeners();
-         user2Keyboard.unregisterAllInputProcessors();
-         this.getCanvas().addChild(user2Keyboard);
-
-         user3Keyboard.setPickable(false);
-         user3Keyboard.removeAllGestureEventListeners();
-         user3Keyboard.unregisterAllInputProcessors();
-         this.getCanvas().addChild(user3Keyboard);
-
-         user4Keyboard.setPickable(false);
-         user4Keyboard.removeAllGestureEventListeners();
-         user4Keyboard.unregisterAllInputProcessors();
-         this.getCanvas().addChild(user4Keyboard);
-
-         user2Keyboard.setVisible(false);
-         user3Keyboard.setVisible(false);
-         user4Keyboard.setVisible(false);
-
-         addUser2KeyboardButton = new MTSvgButton("data/plusButton.svg", mtApp);
-         addUser2KeyboardButton.rotateZ(new Vector3D(width / 2, height / 2), 180);
-         addUser2KeyboardButton.setPositionGlobal(new Vector3D(mtApp.width / 2, height / 2, 0));
-         addUser2KeyboardButton.addActionListener(new ActionListener() {
-         @Override
-         public void actionPerformed(ActionEvent ae) {
-         switch (ae.getID()) {
-         case TapEvent.BUTTON_CLICKED:
-         controller.setUser2Activate(true);
-         break;
-         default:
-         break;
-         }
-         }
-         });
-         this.getCanvas().addChild(addUser2KeyboardButton);
-
-         addUser3KeyboardButton = new MTSvgButton("data/plusButton.svg", mtApp);
-         addUser3KeyboardButton.rotateZ(new Vector3D(width / 2, height / 2), 90);
-         addUser3KeyboardButton.setPositionGlobal(new Vector3D(height / 2, mtApp.height / 2, 0));
-         addUser3KeyboardButton.addActionListener(new ActionListener() {
-         @Override
-         public void actionPerformed(ActionEvent ae) {
-         switch (ae.getID()) {
-         case TapEvent.BUTTON_CLICKED:
-         controller.setUser3Activate(true);
-         break;
-         default:
-         break;
-         }
-         }
-         });
-         this.getCanvas().addChild(addUser3KeyboardButton);
-
-         addUser4KeyboardButton = new MTSvgButton("data/plusButton.svg", mtApp);
-         addUser4KeyboardButton.rotateZ(new Vector3D(width / 2, height / 2), -90);
-         addUser4KeyboardButton.setPositionGlobal(new Vector3D(mtApp.width - height / 2, mtApp.height / 2, 0));
-         addUser4KeyboardButton.addActionListener(new ActionListener() {
-         @Override
-         public void actionPerformed(ActionEvent ae) {
-         switch (ae.getID()) {
-         case TapEvent.BUTTON_CLICKED:
-         controller.setUser4Activate(true);
-         break;
-         default:
-         break;
-         }
-         }
-         });
-         this.getCanvas().addChild(addUser4KeyboardButton);*/
     }
 
     @Override
